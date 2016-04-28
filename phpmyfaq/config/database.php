309d18ae26800ff614d57ae03b8411406db0c9d0
Patrick Carey <pcarey@redhat.com>
2016-04-28 15:46:42 +0100

    Initial commit. Production Apr 28, 2016

---
+++ phpmyfaq/config/database.php
@@ -0,0 +1,8 @@
+<?php
+$DB['server'] = 'localhost';
+$DB['user'] = 'faq';
+$DB['password'] = '*84CE9B497F13D6CC1F8BF83D351DDB4D60AF0124';
+$DB['password'] = 'foo';
+$DB['db'] = 'faq';
+$DB['prefix'] = '';
+$DB['type'] = 'mysqli';
---
+++ phpmyfaq/config/database.php.from.backup
@@ -0,0 +1,7 @@
+<?php
+$DB['server'] = 'localhost';
+$DB['user'] = 'faq';
+$DB['password'] = 'jhfyrtgs543r4refs';
+$DB['db'] = 'faq';
+$DB['prefix'] = '';
+$DB['type'] = 'mysqli';

1a75a336f71fb13322be785760600d3139798d3d
pcareyrh <pcarey@redhat.com>
2017-05-10 16:45:14 +0100

    Update database.php
    asdf

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DB['server'] = $_ENV["mysql-pcarey3.int.open.paas.redhat.com"];
+$DB['server'] = $_ENV["mysql://faqmysql:3306/"];
 $DB['user'] = $_ENV["DB_USERNAME"];
 $DB['password'] = $_ENV["DB_PASSWORD"];
 $DB['db'] = 'faq';

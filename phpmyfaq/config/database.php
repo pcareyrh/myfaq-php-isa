8a4ee73c1284716acab8602efbfb26acc70c9b28
pcareyrh <pcarey@redhat.com>
2017-05-10 16:45:40 +0100

    Update database.php
    wooops

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DB['server'] = $_ENV["mysql://faqmysql:3306/"];
+$DB['server'] = $_ENV["DB_CONNECTION"];
 $DB['user'] = $_ENV["DB_USERNAME"];
 $DB['password'] = $_ENV["DB_PASSWORD"];
 $DB['db'] = 'faq';

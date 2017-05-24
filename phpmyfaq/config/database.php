6c1dcefdc1efb4b3ac09449c18efd57e771cf66b
Patrick Carey <pcarey@redhat.com>
2017-05-24 14:43:57 +0100

    updated mysql connection string

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DB['server'] = $_ENV["DB_CONNECTION"];
+$DB['server'] = $_ENV["MYSQL_SERVICE_HOST"];
 $DB['user'] = $_ENV["DB_USERNAME"];
 $DB['password'] = $_ENV["DB_PASSWORD"];
 $DB['db'] = 'faq';

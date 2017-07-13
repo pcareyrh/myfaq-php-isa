8b7385706f4475c0643af54bc61607bdb8fc30c5
Patrick Carey <pcarey@redhat.com>
2017-07-13 10:53:53 +0100

    renamed vars

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -1,7 +1,7 @@
 <?php
 $DB['server'] = $_ENV["MYSQL_SERVICE_HOST"];
-$DB['user'] = $_ENV["DB_USERNAME"];
-$DB['password'] = $_ENV["DB_PASSWORD"];
+$DB['user'] = $_ENV["MYSQL_USER"];
+$DB['password'] = $_ENV["MYSQL_PASSWORD"];
 $DB['db'] = 'faq';
 $DB['prefix'] = '';
 $DB['type'] = 'mysqli';
